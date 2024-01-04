fbc48245c4dd139d9e00a19c26f7282240df222b
qwality <low.qwality@gmail.com>
2024-01-04 13:45:15 +0100

    avatar

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby "3.3.0"
 gem "haml"
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave'
+
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
 
   validates :first_name, presence: true
   validates :last_name, presence: true
