37267ffc4bfc26e4f8125d38a5c1e32e8e7539c5
qwality <low.qwality@gmail.com>
2024-01-04 15:13:42 +0100

    admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_123040) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_140456) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_04_123040) do
     t.string "last_name"
     t.string "address"
     t.string "avatar"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
