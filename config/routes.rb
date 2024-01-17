c338f41c99d936a8801e51e8b286739099923be4
qwality <low.qwality@gmail.com>
2024-01-17 16:27:49 +0100

    nastylovani

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
   end
   # get 'pages/home'
   # post '/custom_users', to: 'users#create', as: 'custom_users'
-  get 'about', to: 'pages#about'
+  # get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
