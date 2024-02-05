5a9a8e5c8faafc14d052172d8cdb7677f4bec046
qwality <low.qwality@gmail.com>
2024-02-05 22:32:22 +0100

    pridani callbacku na filter sprostych slov do vsech stringovych poli vsech modelu

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'sassc-rails'
 gem 'jquery-rails'
 gem 'faker'
 gem 'cancancan'
+# gem 'profanity_filter'
+gem 'obscenity'
 
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  before_save :filter_profane_words
+
+  def filter_profane_words
+    contains_profane_words = false
+    self.attributes.each do |attribute, value|
+      if value.is_a?(String) and  Obscenity.profane?(value)
+        errors.add(attribute, "obsahuje sprostá slova.")
+        contains_profane_words = true
+      end
+    end
+    throw(:abort) if contains_profane_words
+  end
 end
