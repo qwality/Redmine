1acf71371d6681522adc1f2a815abe05cb9ec4dd
qwality <low.qwality@gmail.com>
2024-02-08 11:52:14 +0100

    jeste lepsi oprava

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,13 +10,19 @@ class Ability
     return unless user.present?
 
     can :read, :all
-    can :manage, Task, user_id: user.id
-    cannot :destroy, Task
-    cannot :create, Task
 
-    can :manage, Project, user_id: user.id
-    cannot :destroy, Project
-    cannot :create, Project
+    can :my_tasks, Task
+    can :my_projects, Project
+
+    can [:update, :edit, :edit_in_table, :show_in_table, :show_in_table_update], Task, user_id: user.id
+
+    # can :manage, Task, user_id: user.id
+    # cannot :destroy, Task
+    # cannot :create, Task
+
+    # can :manage, Project, user_id: user.id
+    # cannot :destroy, Project
+    # cannot :create, Project
 
     return unless user.is_admin?
 
